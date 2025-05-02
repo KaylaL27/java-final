8138cf8d923015eb22937ee38abae2bfff27bb05
Kayla <144965767+KaylaL27@users.noreply.github.com>
2025-05-02 13:25:06 +0000

    finished setting up expenses

--- money/Expenses.java
+++ money/Expenses.java
@@ -1,5 +1,5 @@
 package money;
 
 public class Expenses {
-    
+
 }
--- money/Transactions.java
+++ money/Transactions.java
@@ -1,5 +1,27 @@
 package money;
 
-public class Transactions {
-    
+public abstract class Transactions {
+    protected double amount;
+    protected String category;
+    protected String description;
+
+    public Transactions(double amount, String category, String description){
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract String getType();
 }
